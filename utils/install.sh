4becd1563952562c14cb773caf2f57718c6b55eb
pi <>
2019-03-01 22:22:44 +0000

    Initial file, installation script

---
+++ utils/install.sh
@@ -0,0 +1,11 @@
+
+
+sudo python2.7 -m pip install git+https://github.com/abelectronicsuk/ABElectronics_Python_Libraries.git
+
+sudo apt-get -y install synaptic
+sudo apt-get -y install ipython 
+sudo apt-get -y install jed
+sudo apt-get -y install geany
+sudo apt-get -y install python-qt4
+
+
