d4ed93c7fd9ab845e9fbe7fe17e6d1d98a0c214c
pHox4 <ferrybox@gmail.com>
2019-09-03 17:44:43 +0100

    updated install script

--- utils/install.sh
+++ utils/install.sh
@@ -77,47 +77,8 @@ echo "                        "
 read -p "Skip? Y/[N] " ans
 if [ "$ans" != "Y" ]
 then
-<<<<<<< HEAD
     sudo systemctl start pigpiod
     sudo systemctl enable pigpiod
-#    f="/tmp/pigpio.conf"
-#    g="/etc/init/pigpio.conf"
-#    echo '# pigpio'                       > $f
-#    echo 'description	"PIGPIO daemon"' >> $f
-#    echo 'start on runlevel [2345]'      >> $f
-#    echo 'stop on runlevel [!2345]'      >> $f
-#    echo 'respawn'                       >> $f
-#    echo 'respawn limit 10 5'            >> $f
-#    echo 'umask 022'                     >> $f
-#    echo 'expect stop'                   >> $f
-#    echo 'console none'                  >> $f
-#    echo 'pre-start script'              >> $f
-#    echo '    test -x /usr/bin/pigpiod || { stop; exit 0; }' >> $f
-#    echo 'end script'                    >> $f
-#    echo 'exec /usr/bin/pigpiod'         >> $f
-#    sudo mv $f $g
-=======
-    ## New method via systemd
-    systemctl start pigpiod
-    systemctl enable pigpiod
-    ## Old method via upstart system
-    #f="/tmp/pigpio.conf"
-    #g="/etc/init/pigpio.conf"
-    #echo '# pigpio'                       > $f
-    #echo 'description	"PIGPIO daemon"' >> $f
-    #echo 'start on runlevel [2345]'      >> $f
-    #echo 'stop on runlevel [!2345]'      >> $f
-    #echo 'respawn'                       >> $f
-    #echo 'respawn limit 10 5'            >> $f
-    #echo 'umask 022'                     >> $f
-    #echo 'expect stop'                   >> $f
-    #echo 'console none'                  >> $f
-    #echo 'pre-start script'              >> $f
-    #echo '    test -x /usr/bin/pigpiod || { stop; exit 0; }' >> $f
-    #echo 'end script'                    >> $f
-    #echo 'exec /usr/bin/pigpiod'         >> $f
-    #sudo mv $f $g
->>>>>>> 92351c4ac5afc5b0d74b0a3795f40377426d1219
 fi
 #--------------------------------------------------------------------------
 # Install Autostart
@@ -135,11 +96,11 @@ then
     then
         mkdir -p "/home/pi/.config/autostart"
     fi
-    echo '[Desktop Entry]'                                  > $f
-    echo 'Type=Application'                                >> $f
-    echo 'NAME=pHox'                                       >> $f
-    echo 'Exec=sudo /usr/bin/python /home/pi/pHox/pHox.py' >> $f
-    echo 'X-GNOME-Autostart-enabled=true'                  >> $f
+    echo '[Desktop Entry]'                                     >  $f
+    echo 'Type=Application'                                    >> $f
+    echo 'NAME=pHox'                                           >> $f
+    echo 'Exec=sudo /usr/bin/python /home/pi/pHox/pHox_gui.py' >> $f
+    echo 'X-GNOME-Autostart-enabled=true'                      >> $f
     mv $f $g
 fi
 #--------------------------------------------------------------------------
