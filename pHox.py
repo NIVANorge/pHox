fac9f952ca516497735392c4be1a624b2e7b7477
lisapro <elisaveta.pro@gmail.com>
2020-01-27 16:35:22 +0100

    chagne wait time

--- pHox.py
+++ pHox.py
@@ -30,7 +30,7 @@ class Spectro_seabreeze(object):
     def set_integration_time(self,time_millisec):
         microsec = time_millisec * 1000
         self.spec.integration_time_micros(microsec)  # 0.1 seconds
-        time.sleep(5)
+        time.sleep(time_millisec/1.e3)
 
     def get_wavelengths(self):
         #wavelengths in (nm) corresponding to each pixel of the spectrom
