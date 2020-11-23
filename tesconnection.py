834bdcfbe13c85312b7366854da34c66b90cd082
co3 box <ferrybox@niva.no>
2020-11-23 18:22:05 +0000

    test connection function

--- tesconnection.py
+++
@@ -1,8 +0,0 @@
-import seabreeze
-seabreeze.use("cseabreeze")
-from seabreeze.spectrometers import Spectrometer
-from seabreeze.spectrometers import list_devices
-first = Spectrometer.from_first_available()
-print (first)
-all = list_devices()
-print (all)
